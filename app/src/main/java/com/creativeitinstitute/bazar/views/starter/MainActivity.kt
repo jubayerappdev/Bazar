7b1728c92f8a8ba7cc19c6885ae9aad31f89810d
Md. Mehedi Hasan <108575130+mehedidevs@users.noreply.github.com>
2024-04-23 19:51:45 +0600

    mvvm commit

--- app/src/main/java/com/creativeitinstitute/bazar/views/starter/MainActivity.kt
+++ app/src/main/java/com/creativeitinstitute/bazar/views/starter/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.creativeitinstitute.bazar
+package com.creativeitinstitute.bazar.views.starter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.creativeitinstitute.bazar.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/creativeitinstitute/bazar/views/starter/StartFragment.kt
@@ -0,0 +1,14 @@
+package com.creativeitinstitute.bazar.views.starter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class StartFragment : Fragment() {
+
+
+
+}
