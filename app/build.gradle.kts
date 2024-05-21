2ee8a622abfba69d008f31645c4f1375b9fc9648
jubo445 <juboraj335.ja@gmail.com>
2024-05-21 20:31:44 +0600

    menu added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics:22.0.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.activity:activity:1.9.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/creativeitinstitute/bazar/views/dashboard/seller/SellerDashboard.kt
+++ app/src/main/java/com/creativeitinstitute/bazar/views/dashboard/seller/SellerDashboard.kt
@@ -1,6 +1,10 @@
 package com.creativeitinstitute.bazar.views.dashboard.seller
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -9,12 +13,18 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.creativeitinstitute.bazar.R
 import com.creativeitinstitute.bazar.databinding.ActivitySellerDashboardBinding
+import com.creativeitinstitute.bazar.views.starter.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SellerDashboard : AppCompatActivity() {
     lateinit var binding : ActivitySellerDashboardBinding
     lateinit var navController: NavController
+
+    @Inject
+    lateinit var jAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,5 +52,30 @@ class SellerDashboard : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_top, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.menu_logout ->{
+                jAuth.signOut()
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+            R.id.menu_settings->{
+                Toast.makeText(this,"Settings Clicked", Toast.LENGTH_LONG).show()
+            }
+            R.id.menu_report->{
+                Toast.makeText(this,"Report Clicked", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 }
